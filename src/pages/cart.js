c9b6ba1b859822db93a4003ca0cf647dba753695
Jhon Natera <jnnatera@uninorte.edu.co>
2022-06-07 11:13:52 -0500

    prueba

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -31,7 +31,7 @@ export default function Cart (){
             window.localStorage.removeItem("cart")
             navigate("/")
         } else {
-            navigate("/login")
+            navigate("/article")
         }
     }
     return(
